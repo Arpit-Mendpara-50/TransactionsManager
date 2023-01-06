50076b6d7298d41079f28addfac5d0ab704186e7
Arpit Mendpara <arpit.mendpara@ibigroup.com>
2023-01-06 14:35:21 +0530

    Initial Commit

---
+++ TransactionManager/TransactionManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionManagerApp.swift
+//  TransactionManager
+//
+//  Created by Arpit Mendpara on 2023-01-06.
+//
+
+import SwiftUI
+
+@main
+struct TransactionManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
