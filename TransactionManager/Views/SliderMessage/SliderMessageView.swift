39e14242a97dabb2c1bdabf38ef69e70f9a250aa
Arpit Mendpara <arpit.mendpara@ibigroup.com>
2024-02-01 18:18:29 -0500

    Updated the slider message UI.

--- TransactionManager/Views/SliderMessage/SliderMessageView.swift
+++ TransactionManager/Views/SliderMessage/SliderMessageView.swift
@@ -13,21 +13,8 @@ struct SliderMessageView: View {
     var closeAction: (() -> Void)? = nil
     var body: some View {
         VStack{
-            VStack(spacing: 0){
-                HStack {
-                VStack {
-                    
-                }.frame(width: 10, height: 10)
-                    .background(title == "Success" ? Color.green : Color.red)
-                    .shadow(radius: 5)
-                    Spacer()
-                    VStack {
-                        
-                    }.frame(width: 10, height: 10)
-                        .background(title == "Success" ? Color.green : Color.red)
-                        .shadow(radius: 4)
-                }
-                .padding(.horizontal, 30)
+            VStack(spacing: 10){
+                Spacer().frame(height: ScreenSize.safeTop())
                 HStack {
                     Spacer()
                     Text(message)
@@ -35,13 +22,11 @@ struct SliderMessageView: View {
                         .font(.system(size: 20, weight: .bold))
                     Spacer()
                 }
-                .frame(minHeight: 70)
-                .background(title == "Success" ? Color.green : Color.red)
-                .foregroundStyle(Color.white)
-                .clipShape(RoundedRectangle(cornerRadius: 5))
-                .shadow(radius: 5)
             }
-            .padding(.horizontal)
+            .background(title == "Success" ? Color.green : Color.red)
+            .foregroundStyle(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            .shadow(radius: 5)
             Spacer()
         }
         .onAppear {
@@ -51,7 +36,7 @@ struct SliderMessageView: View {
                 }
             })
         }
-        .ignoresSafeArea(edges: .bottom)
+        .ignoresSafeArea(edges: .all)
     }
 }
 
